12af357a603d2a1f51b767a1de0abef737e841f4
JAragon <jaragon@spcdatapro.com>
2018-04-25 23:13:59 -0600

    Rediseño del reporte de Contrato Inactivo según requerimientos de Sandra. 25/04/2018 23:13

--- php/rptcontratoinactivo.php
+++ php/rptcontratoinactivo.php
@@ -30,14 +30,18 @@ $app->post('/contrato', function(){
     $query = "SELECT DATE_FORMAT(NOW(), '%d/%m/%Y %H:%i:%s') AS fecha";
     $info->generales = $db->getQuery($query)[0];
 
-    $query = "SELECT a.id, a.idcliente, b.nombre AS cliente, b.nombrecorto AS abreviacliente, a.nocontrato, a.abogado, DATE_FORMAT(a.fechainicia, '%d/%m/%Y') AS fechainicia, DATE_FORMAT(a.fechavence, '%d/%m/%Y') AS fechavence, ";
-    $query.= "a.inactivo, DATE_FORMAT(a.fechainactivo, '%d/%m/%Y') AS fechainactivo, a.idmoneda, c.simbolo AS moneda, a.idmonedadep, d.simbolo AS monedadep, a.deposito, a.idcuentac, a.reciboprov, a.idperiodicidad, e.descperiodicidad AS periodicidad, ";
-    $query.= "IF(a.documento = 1, 'FACTURA', 'RECIBO') AS documento, IF(a.subarrendado = 1, 'Sí', 'No') AS subarrendado, a.idtipocliente, f.desctipocliente AS tipocliente, a.idempresa, g.nomempresa AS empresa, g.abreviatura AS abreviaempresa, ";
-    $query.= "a.idproyecto, h.nomproyecto AS proyecto, UnidadesPorContrato(a.id) AS unidades, a.idtipoipc, i.descripcion AS tipoincremento, IF(a.cobro = 1, 'Sí', 'No') AS cobrovencido, DATE_FORMAT(a.plazofdel, '%d/%m/%Y') AS plazofdel, ";
-    $query.= "DATE_FORMAT(a.plazofal, '%d/%m/%Y') AS plazofal, a.prescision, a.observaciones ";
-    $query.= "FROM contrato a INNER JOIN cliente b ON b.id = a.idcliente INNER JOIN moneda c ON c.id = a.idmoneda INNER JOIN moneda d ON d.id = a.idmonedadep INNER JOIN periodicidad e ON e.id = a.idperiodicidad ";
-    $query.= "INNER JOIN tipocliente f ON f.id = a.idtipocliente INNER JOIN empresa g ON g.id = a.idempresa INNER JOIN proyecto h ON h.id = a.idproyecto INNER JOIN tipoipc i ON i.id = a.idtipoipc ";
-    $query.= "WHERE a.id = $d->idcontrato";
+    $query = "SELECT e.nomempresa AS empresa, d.nombre AS cliente, d.nombrecorto AS abreviacliente, b.nomproyecto AS ubicacion, UnidadesPorContrato(a.id) AS unidades, a.nocontrato, a.abogado, DATE_FORMAT(a.fechainicia, '%d/%m/%Y') AS inicia,
+    DATE_FORMAT(a.fechavence, '%d/%m/%Y') AS vence, DATE_FORMAT(a.fechainactivo, '%d/%m/%Y') AS inactivodesde,
+    (SELECT CONCAT(MONTH(MAX(fechacobro)), '/', YEAR(MAX(fechacobro))) FROM cargo WHERE facturado = 1 AND anulado = 0 AND idcontrato = $d->idcontrato) AS ultimocobro,
+    c.simbolo AS monedadep, FORMAT(a.deposito, 2) AS deposito, a.reciboprov AS recibo,
+    IFNULL((SELECT CONCAT(x.simbolo, ' ', FORMAT((SUM(z.total) - SUM(IFNULL(y.monto, 0.00))), 2)) FROM factura z LEFT JOIN detcobroventa y ON z.id = y.idfactura LEFT JOIN moneda x ON x.id = z.idmoneda WHERE z.idcontrato = $d->idcontrato AND z.pagada = 0), 0.00) AS saldo,
+    a.observaciones
+    FROM contrato a
+    LEFT JOIN proyecto b ON b.id = a.idproyecto
+    LEFT JOIN moneda c ON c.id = a.idmonedadep
+    LEFT JOIN cliente d ON d.id = a.idcliente
+    LEFT JOIN empresa e ON e.id = a.idempresa
+    WHERE a.id = $d->idcontrato";
     $info->contrato = $db->getQuery($query)[0];
 
     print json_encode($info);
