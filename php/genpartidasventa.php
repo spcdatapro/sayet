387d0ef36b997f2cd4616d144a24830a8f194918
JAragon <jaragon@spcdatapro.com>
2018-06-12 10:29:20 -0600

    Regeneración de partidas contables de facturas de venta con Ids 6106, 6110, 6111, 6113, 6105, 5888, 6109, 5876

--- php/genpartidasventa.php
+++ php/genpartidasventa.php
@@ -108,7 +108,7 @@ $app->get('/generar', function(){
 $app->get('/regen', function(){
     $db = new dbcpm();
     $origen = 3;
-    $ids = "5369, 5677";
+    $ids = "6106, 6110, 6111, 6113, 6105, 5888, 6109, 5876";
     echo "<!DOCTYPE html><html><head><meta charset='UTF-8'></head><body><small><h3>Regeneración de facturas específicas</h3><h2>$ids</h2>";
 
     $query = "DELETE FROM detallecontable WHERE origen = $origen AND idorigen IN($ids)";
