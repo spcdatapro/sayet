e0d4c331994f93c4c220b703e8b039ba2f0bae92
JAragon <jaragon@spcdatapro.com>
2018-07-02 17:04:01 -0600

    Cierre y apertura anual. 02/07/2018 17:03

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@
         'cpm.rptfactsemitidasctrl', 'cpm.rptarbolsrvcctrl', 'cpm.rptivaventactrl', 'cpm.dropdigitsfltr', 'cpm.rptrecclictrl',
         'cpm.formsretventactrl', 'cpm.rptretenedoresctrl', 'cpm.rptasistelibrosctrl', 'cpm.estatuspresupuestosrvc', 'cpm.rptdescuadresctrl',
         'cpm.rptdetcontventasctrl', 'cpm.rptintegractacontctrl', 'cpm.rptarchivobictrl', 'cpm.factsparqueosrvc', 'cpm.tpo',
-        'cpm.planillasrvc', 'cpm.genpagosplnctrl', 'cpm.rptcompaguactrl', 'cpm.rptfactsparqueoctrl'
+        'cpm.planillasrvc', 'cpm.genpagosplnctrl', 'cpm.rptcompaguactrl', 'cpm.rptfactsparqueoctrl', 'cpm.cierreanualsrvc', 'cpm.cierreanualctrl'
     ]);
 
     app.config(['$routeProvider', 'desktopNotificationProvider', function ($routeProvider, desktopNotificationProvider) {
