c933c6d9fd5720bc879e0932864a7d2ea4a7e8b8
Raul C <39995984+rcspc@users.noreply.github.com>
2018-07-30 18:47:22 -0600

    Correccion de antiguedad de clientes

--- php/rptanticliente.php
+++ php/rptanticliente.php
@@ -84,7 +84,7 @@ $app->post('/rptanticli', function(){
             $querydet2 = " order by c.fecha
                         ) as a
                         left join(
-                            select orden,cliente,venta,fecha,documento,tipo,monto,codigo,tc_cambio from (
+                            select orden,cliente,venta,fecha,documento,tipo,monto,codigo,tc_cambio,idpago from (
 
                                 SELECT 2 as orden,a.idcliente as cliente,a.id as venta,c.fecha,d.numero as documento,'R' as tipo, (b.monto) as monto,
                                     'Q' as codigo,a.tipocambio as tc_cambio, b.id as idpago
@@ -107,7 +107,7 @@ $app->post('/rptanticli', function(){
 
             $query = "Select distinct idempresa, empresa from (".$querydet1.$querydet2.$querydet3.") as a order by empresa;";
 
-            //echo $query;
+            echo $query;
 
             $empcli = $db->getQuery($query);
 
