019ebbd6f9bf51b437628b8ff95475eaa660d0c5
Luis Miguel Tziná <desarrollo@c807.com>
2018-08-29 11:09:14 -0600

    Correccion en filtro en reporte de proyección de préstamos

--- pln/php/models/General.php
+++ pln/php/models/General.php
@@ -285,12 +285,16 @@ class General extends Principal
 
 		if (isset($args['finalizado'])) {
 			if ($args['finalizado'] == 0) {
-				$where['OR'] = [
-					'finalizado[=]' => 0,
-					'AND' => [
-						'liquidacion[<>]' => array(formatoFecha($args['fal'], 5), $args['fal'])
-					]
-				];
+				if (elemento($args, 'fal')) {
+					$where['OR'] = [
+						'finalizado[=]' => 0,
+						'AND' => [
+							'liquidacion[<>]' => array(formatoFecha($args['fal'], 5), $args['fal'])
+						]
+					];
+				} else {
+					$where['finalizado[=]'] = $args['finalizado'];
+				}
 			} else {
 				$where['finalizado[=]'] = $args['finalizado'];
 			}
