8c384a025a5329bd14330045ca8618a270510511
JAragon <jaragon@spcdatapro.com>
2018-11-22 10:52:07 -0600

    Descarga de ficha de cliente a excel. 22/11/2018 10:52.

--- js/controllers/rptfichaclientectrl.js
+++ js/controllers/rptfichaclientectrl.js
@@ -2,7 +2,7 @@
 
     var rptfichaclientectrl = angular.module('cpm.rptfichaclientectrl', []);
 
-    rptfichaclientectrl.controller('rptFichaClienteCtrl', ['$scope', 'clienteSrvc', 'jsReportSrvc', function($scope, clienteSrvc, jsReportSrvc){
+    rptfichaclientectrl.controller('rptFichaClienteCtrl', ['$scope', 'clienteSrvc', 'jsReportSrvc', '$filter', function($scope, clienteSrvc, jsReportSrvc, $filter){
 
         $scope.params = { idcliente: undefined };
         $scope.clientes = [];
@@ -12,6 +12,13 @@
         var test = false;
         $scope.getRptFichaCliente = function(){
             jsReportSrvc.getPDFReport(test ? 'HJfp-erkg' : 'SyZ8gmHkx', $scope.params).then(function(pdf){ $scope.content = pdf; });
+        };
+
+        $scope.getRptFichaClienteExcel = function(){
+            jsReportSrvc.getReport(test ? 'ryocwUEC7' : 'ryocwUEC7', $scope.params).then(function(result){
+                var file = new Blob([result.data], {type: 'application/vnd.ms-excel'});
+                saveAs(file, 'Ficha_Cliente_' + $filter('padNumber')(+$scope.params.idcliente, 5) + '.xlsx');
+            });
         };
 
         $scope.resetParams = function(){ $scope.params = { idcliente: undefined }; };
