ae4528154af115fe43baed12a3d57a6c824ab92f
JAragon <jaragon@spcdatapro.com>
2019-08-05 16:39:25 -0600

    Regeneración de facturas de agua de IBEX. 05/08/2019 16:39.

--- php/genpartidasventa.php
+++ php/genpartidasventa.php
@@ -108,7 +108,7 @@ $app->get('/generar', function(){
 $app->get('/regen', function(){
     $db = new dbcpm();
     $origen = 3;
-    $ids = "8215, 8216";
+    $ids = "9477, 9470, 9472, 9473, 9471, 9474, 9468, 9476, 9469, 9475";
     echo "<!DOCTYPE html><html><head><meta charset='UTF-8'></head><body><small><h3>Regeneración de facturas específicas</h3><h2>$ids</h2>";
 
     $query = "DELETE FROM detallecontable WHERE origen = $origen AND idorigen IN($ids)";
