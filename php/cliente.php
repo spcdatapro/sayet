02d043daaace5115c9a44a758e85cb60f2b560f3
JAragon <jaragon@spcdatapro.com>
2019-08-26 15:20:39 -0600

    Fix para que aparezca el numero de contrato cuando no hay unidad asignada a un contrato. 26/08/2019 15:20.

--- php/cliente.php
+++ php/cliente.php
@@ -275,7 +275,7 @@ $app->get('/lstcontratos/:idcliente', function($idcliente){
 $app->get('/lstcontemp/:idcliente/:idempresa', function($idcliente, $idempresa){
     $db = new dbcpm();
     $query = "SELECT a.id, a.idcliente, a.nocontrato, a.abogado, a.inactivo, a.fechainactivo, a.fechainicia, a.fechavence, a.nuevarenta, a.nuevomantenimiento, a.idmoneda, b.simbolo AS moneda, ";
-    $query.= "a.idempresa, c.nomempresa AS empresa, a.deposito, a.idproyecto, d.nomproyecto AS proyecto, a.idunidad, UnidadesPorContrato(a.id) AS unidad, a.retiva, a.prorrogable, a.retisr, ";
+    $query.= "a.idempresa, c.nomempresa AS empresa, a.deposito, a.idproyecto, d.nomproyecto AS proyecto, a.idunidad, IFNULL(UnidadesPorContrato(a.id), CONCAT('Contrato #', a.nocontrato)) AS unidad, a.retiva, a.prorrogable, a.retisr, ";
     $query.= "a.documento, a.adelantado, a.subarrendado, a.idtipocliente, f.desctipocliente AS tipocliente, a.idcuentac, a.observaciones, a.idmonedadep, ";
     $query.= "h.simbolo AS monedadep, a.reciboprov, a.idperiodicidad, a.idtipoipc, a.cobro, a.plazofdel, a.plazofal, a.prescision ";
     $query.= "FROM contrato a LEFT JOIN moneda b ON b.id = a.idmoneda LEFT JOIN empresa c ON c.id = a.idempresa LEFT JOIN proyecto d ON d.id = a.idproyecto ";
