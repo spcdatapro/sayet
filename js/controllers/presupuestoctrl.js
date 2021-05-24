9d005894ca3897b59a46bbbe8e196e164423dbad
sponce <sebas6nin@gmail.com>
2021-05-24 14:50:57 -0600

    Cambios detalle ot. 24/05/2021, 14:50.

--- js/controllers/presupuestoctrl.js
+++ js/controllers/presupuestoctrl.js
@@ -138,11 +138,12 @@
                 $scope.lbl.id = $scope.presupuesto.id ;
                 $scope.lbl.proyecto = ' Proyecto: ' + ($filter('getById')($scope.proyectos, $scope.presupuesto.idproyecto)).nomproyecto;
                 $scope.lbl.empresa = ' Empresa: ' + ($filter('getById')($scope.empresas, $scope.presupuesto.idempresa)).nomempresa;
-                $scope.lbl.notas = ' Notas: ' + $scope.presupuesto.notas
+                $scope.lbl.notas = ' Concepto: ' + $scope.presupuesto.notas.substring(0,20);
                 $scope.lbl.tipgasto = ' Tipo gasto: ' + ($filter('getById')($scope.tiposgasto, $scope.presupuesto.idtipogasto)).desctipogast;
                 $scope.lbl.montopres = ' Monto Presupuestado: ' + ($filter('getById')($scope.monedas, $scope.presupuesto.idmoneda)).simbolo + $scope.presupuesto.montoot;
                 $scope.lbl.montogas = ' Monto Gastado: ' + ($filter('getById')($scope.monedas, $scope.presupuesto.idmoneda)).simbolo + $scope.presupuesto.montogastado;
                 $scope.lbl.avance = ' Avance: ' + $scope.presupuesto.avanceot;
+                $scope.lbl.desc = ' Descripcion: ' + $scope.presupuesto.notas;
                 $scope.confGrpBtn('grpBtnPresupuesto', false, false, true, true, true, false, false);
                 $scope.sl.presupuesto = true;
                 if (movertab) {
