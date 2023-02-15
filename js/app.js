d235b31acc00537f301cb9dcb0ff1d7ca3f69db5
sponce <sebas6nin@gmail.com>
2023-02-15 09:21:09 -0600

    Nuevo formato libro salarios. 15/02/2023, 9:21.

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@
         'cpm.plnpagoboletoornatoctrl', 'cpm.scrolldetector', 'cpm.rptplncatemplevalctrl', 'cpm.trangenchqotsctrl', 'cpm.socketiosrvc',
         'cpm.tranlecturasaguactrl', 'cpm.tipoimpchqctrl', 'cpm.confaprotsctrl', 'cpm.rptviewer', 'cpm.otsimplificadoctrl',
         'cpm.testctrl', 'cpm.rptplnivsctrl', 'cpm.rptarchivondctrl', 'cpm.rptcontinactctrl', 'cpm.trangenchqotscontctrl', 'cpm.rptcheqsinfactctrl',
-        'cpm.testsrvc', 'cpm.rpttoptenprovctrl', 'cpm.rptrecclimenctrl', 'cpm.rptcontcrea', 'cpm.rptcorrelageren'
+        'cpm.testsrvc', 'cpm.rpttoptenprovctrl', 'cpm.rptrecclimenctrl', 'cpm.rptcontcrea', 'cpm.rptcorrelageren', 'cpm.rptlibsalario'
     ]);
 
     app.config(['$routeProvider', 'desktopNotificationProvider', function ($routeProvider, desktopNotificationProvider) {
