220c07065547cc28be59e632cdde68e9b55999d0
sponce <sebas6nin@gmail.com>
2023-07-10 09:22:23 -0600

    Ajuste para que las facturas exentas aparezcan en la columna de exentas. 10/07/2023, 9:22.

--- php/rptlibroventas.php
+++ php/rptlibroventas.php
@@ -20,16 +20,16 @@ $app->post('/rptlibventas', function(){
     $query = "SELECT a.fecha AS fechafactura, IF(a.fecha >= '2020-08-01', c.siglasfel, c.siglas) AS tipodocumento, a.serieadmin, a.numeroadmin, a.serie, a.numero AS documento, ";
 	$query.= "IF(a.anulada = 0, TRIM(a.nit), '0') AS nit, ";
     $query.= "substr(IF(a.anulada = 0, TRIM(a.nombre), 'ANULADA'),1,35) AS cliente, ";
-    $query.= "IF(a.anulada = 0, IF(a.idtipoventa IN(1, 2, 4), IF(c.generaiva = 0 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9), ROUND((a.subtotal - a.noafecto), 2), 0.00), 0.00), 0.00) AS exento, ";
-    $query.= "IF(a.anulada = 0, IF(a.idtipoventa = 4, IF(c.generaiva = 1 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9) AND a.importeexento = 0, ROUND((a.total - a.noafecto - a.importeiva), 2), 0.00), 0.00), 0.00) AS activo, ";
-    $query.= "IF(a.anulada = 0, IF(a.idtipoventa = 1, IF(c.generaiva = 1 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9) AND a.importeexento = 0, ROUND((a.total - a.noafecto - a.importeiva), 2), 0.00), 0.00), 0.00) AS bien, ";    	
-	$query.= "IF(a.anulada = 0, IF(a.idtipoventa = 2, IF(c.generaiva = 1 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9) AND a.importeexento = 0, ROUND(a.subtotal - a.importeiva, 2), 0.00), 0.00), 0.00) AS servicio, ";	
+    $query.= "IF(a.anulada = 0, IF(a.idtipoventa IN(1, 2, 4), IF(a.exentoiva = 1 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9), ROUND((a.subtotal - a.noafecto), 2), 0.00), 0.00), 0.00) AS exento, ";
+    $query.= "IF(a.anulada = 0, IF(a.idtipoventa = 4, IF(a.exentoiva = 0 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9) AND a.importeexento = 0, ROUND((a.total - a.noafecto - a.importeiva), 2), 0.00), 0.00), 0.00) AS activo, ";
+    $query.= "IF(a.anulada = 0, IF(a.idtipoventa = 1, IF(a.exentoiva = 0 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9) AND a.importeexento = 0, ROUND((a.total - a.noafecto - a.importeiva), 2), 0.00), 0.00), 0.00) AS bien, ";    	
+	$query.= "IF(a.anulada = 0, IF(a.idtipoventa = 2, IF(a.exentoiva = 0 AND a.idtipofactura IN (1, 2, 3, 4, 5, 7, 8, 9) AND a.importeexento = 0, ROUND(a.subtotal - a.importeiva, 2), 0.00), 0.00), 0.00) AS servicio, ";	
 	$query.= "IF(a.anulada = 0, ROUND(a.importeiva, 2), 0.00) AS iva, IF(a.anulada = 0, ROUND(a.subtotal, 2), 0.00) AS totfact, a.idtipofactura, IF(a.anulada = 0, a.importeexento, 0.00) AS importeexento, ";
 	$query.= "IF(a.idtipofactura != 9, null, 1) AS negativo, IF(a.idtipofactura != 9, 1, null) AS venta ";
     $query.= "FROM factura a LEFT JOIN contrato b ON b.id = a.idcontrato LEFT JOIN tipofactura c ON c.id = a.idtipofactura LEFT JOIN cliente d ON d.id = a.idcliente ";
     $query.= "WHERE a.idtipoventa <> 5 AND c.id <> 5 AND a.idempresa = $idempresa AND a.mesiva = $mes AND YEAR(a.fecha) = $anio AND LENGTH(a.serie) > 0 AND LENGTH(a.numero) > 0 ";
 	$query.= "ORDER BY ".((int)$d->alfa > 0 ? "8, 1, 3, 4, 5, 6" : "1, 3, 4, 5, 6, 8");	
-	//print $query;	
+	// print $query;	
 	$detlbventa = $db->getQuery($query);
 	
 	$libventas = array();
@@ -55,7 +55,7 @@ $app->post('/rptlibventas', function(){
 				'totfact' => number_format($dlbv->totfact * $factor, 2, '.', ''),
 				'serieadmin' => $dlbv->serieadmin,
 				'numeroadmin' => $dlbv->numeroadmin,
-				'exento' => $dlbv->importeexento,
+				// 'exento' => $dlbv->importeexento,
 				'negativa' => $dlbv->negativo, 
 				'venta' => $dlbv->venta
 			)
