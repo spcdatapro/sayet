67e54d5d6e4fa57a0a53aa7afc649ad62065cf26
sponce <sebas6nin@gmail.com>
2023-10-04 13:06:57 -0600

    Revisar si la fecha de baja se encuentra en el mes. 10/04/2023, 13:06.

--- pln/php/models/Nomina.php
+++ pln/php/models/Nomina.php
@@ -117,19 +117,36 @@ class Nomina extends Principal
 	{
 		$fecha = $args['fecha'];
 		$tmpFecha = new DateTime($fecha);
+		$baja = $fecha->format('Y-m-01');
 
 		$this->limpiar_nomina($args);
 
+		// if (elemento($args, 'empresa')) {
+		// 	$condicion = ["AND" => [
+		// 		"idempresadebito" => $args['empresa'],
+		// 		"activo" => 1
+		// 	]];
+		// } else {
+		// 	$condicion = ['activo' => 1];
+		// }
+
+		// $tmp = $this->db->select('plnempleado', ['*'], $condicion);
+
+		$where = "";
+		
 		if (elemento($args, 'empresa')) {
-			$condicion = ["AND" => [
-				"idempresadebito" => $args['empresa'],
-				"activo" => 1
-			]];
-		} else {
-			$condicion = ['activo' => 1];
+			$where .= "AND idempresadebito = {$args['empresa']} ";
 		}
 
-		$tmp = $this->db->select('plnempleado', ['*'], $condicion);
+		$sql = <<<EOT
+		SELECT 
+			*
+		FROM
+			plnempleado
+		WHERE (activo = 1 OR baja >= $baja) {$where}
+		EOT;
+		
+		$tmp = $this->db->query($sql)->fetchAll();
 
 		foreach ($tmp as $row) {
 			$insertar = FALSE;
