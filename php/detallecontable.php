c61c627a5f02c65f7ad2202ff6100f6c0778ff35
sponce <sebas6nin@gmail.com>
2025-01-17 10:13:45 -0600

    Fix para eliminar partidas contables. 17/01/2025, 10:13.

--- php/detallecontable.php
+++ php/detallecontable.php
@@ -83,10 +83,10 @@ $app->post('/d', function(){
     if(isset($d->idcompra)) {
         $idcuenta = $db->getOneField("SELECT idcuenta FROM detallecontable WHERE id = $d->id");
         $existe = $db->getOneField("SELECT id FROM compraproyecto WHERE idcompra = $d->idcompra AND idcuentac = $idcuenta"); 
-    }
 
-    if($existe > 0) {
-        $db->doQuery("DELETE FROM compraproyecto WHERE id = $existe");
+        if($existe > 0) {
+            $db->doQuery("DELETE FROM compraproyecto WHERE id = $existe");
+        }
     }
 
     $query = "DELETE FROM detallecontable WHERE id = ".$d->id;
