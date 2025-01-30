496dc4f4e12f3188ea7127a1baeb4991b9ec519c
sponce <sebas6nin@gmail.com>
2025-01-30 15:33:04 -0600

    Hacer el calculo de los incrementos. 30/01/2025, 15:21.

--- js/controllers/rptincdecctrl.js
+++ js/controllers/rptincdecctrl.js
@@ -6,6 +6,7 @@
 
         $scope.params = {fdel: moment().startOf('month').toDate() , fal: moment().endOf('month').toDate(), tipo: '1'};
         $scope.content = `${window.location.origin}/sayet/blank.html`;
+        $scope.cargando = false;
         //$scope.empresas = [];
         //$scope.proyectos = [];
         //empresaSrvc.lstEmpresas().then(function(d){ $scope.lasEmpresas = d; $scope.lasEmpresas.push({ id: '', nomempresa: 'Todas las empresas' }); });
@@ -13,9 +14,12 @@
 
         var test = false;
         $scope.getRptIncDec = function(){
+            $scope.cargando = true;
             $scope.params.fdelstr = moment($scope.params.fdel).format('YYYY-MM-DD');
             $scope.params.falstr = moment($scope.params.fal).format('YYYY-MM-DD');
-            jsReportSrvc.getPDFReport(test ? 'rJ6LbOcGx' : 'Bypkjqczx', $scope.params).then(function(pdf){ $scope.content = pdf; });
+            jsReportSrvc.getPDFReport(test ? 'rJ6LbOcGx' : 'Bypkjqczx', $scope.params)
+            .then(function(pdf){ $scope.content = pdf })
+            .then(function(){ $scope.cargando = false });
         };
 
         $scope.resetParams = function(){ $scope.params = {fdel: moment().startOf('month').toDate() , fal: moment().endOf('month').toDate(), tipo: '1'}; };
