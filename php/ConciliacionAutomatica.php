284cfc4568ef3666b3a949b13b18bb36202cc1c9
sponce <sebas6nin@gmail.com>
2025-04-11 13:38:25 -0600

    MT940

--- php/ConciliacionAutomatica.php
+++ php/ConciliacionAutomatica.php
@@ -217,7 +217,7 @@ class ConciliacionAutomatica
                 $grabado = true;
             } else {
                 $grabado = true;
-                $this->existia = true;
+                // $this->existia = true;
             }
         }
         return $grabado;
@@ -268,16 +268,16 @@ class ConciliacionAutomatica
                 }
 
                 if (count($errores) === 0) {
-                    if (!$this->existia) {
+                    // if (!$this->existia) {
                         unset($datos['json']);
                         $datos['exito'] = true;
                         $datos['mensaje'] = 'Lista de archivos MT940.';
-                    } else {
-                        unset($datos['json']);
-                        $datos['exito'] = true;
-                        $datos['mensaje'] = 'No existen archivos nuevos, favor intentar mas tarde.';
-                        $archivos = [];
-                    }
+                    // } else {
+                        // unset($datos['json']);
+                        // $datos['exito'] = true;
+                        // $datos['mensaje'] = 'No existen archivos nuevos, favor intentar mas tarde.';
+                        // $archivos = [];
+                    // }
                 } else {
                     $datos['mensaje'] = implode('. ', $errores);
                 }
