a62a5606c37d66588141ff6cac651dbe19a41fc2
sponce <sebas6nin@gmail.com>
2025-07-11 09:56:21 -0600

    Traer el empleado de la forma correcta

--- js/controllers/rptplnivsctrl.js
+++ js/controllers/rptplnivsctrl.js
@@ -8,7 +8,21 @@
         $scope.empleados = [];
         $scope.content = `${window.location.origin}/sayet/blank.html`;
 
-        empServicios.buscar({'sin_limite':1}).then((res) => $scope.empleados = res.resultados);
+        empServicios.buscar({'sin_limite':1}).then((res) => {
+            res.resultados.forEach(value => {
+                value.segundonombre = value.segundonombre ? value.segundonombre : '';
+                value.tercernombre = value.tercernombre ? value.tercernombre : '';
+
+                value.nombre = value.primernombre + ' ' + value.segundonombre + ' ' + value.tercernombre;
+
+                value.primerapellido = value.primerapellido ? value.primerapellido : '';
+                value.segundoapellido = value.segundoapellido ? value.segundoapellido : '';
+                value.apellidocasada = value.apellidocasada ? value.apellidocasada : '';
+
+                value.apellidos = value.primerapellido + ' ' + value.segundoapellido + ' ' + value.apellidocasada;
+            });
+            $scope.empleados = res.resultados
+        });
 
         const test = false;
         $scope.getReporte = () => {
