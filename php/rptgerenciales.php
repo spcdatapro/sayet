07fa827a49939a4d728be765627f11ddba77ebe5
sponce <sebas6nin@gmail.com>
2025-07-22 09:14:56 -0600

    Ordernar por numero

--- php/rptgerenciales.php
+++ php/rptgerenciales.php
@@ -731,7 +731,7 @@ $app->post('/control_ingresos', function () {
                 WHERE
                     a.fecha = '$d->fechastr' AND b.gruposumario IN($d->tipo)
                         AND a.tipotrans IN('R', 'D')
-                GROUP BY a.id ORDER BY 2 , 6 , 10 , 8 DESC";
+                GROUP BY a.id ORDER BY 2 , 6 , i.ordenalt , a.numero";
     } else {
         $query = "SELECT 
                 a.id,
