b890e0e6568c708de4dad244f12b55eb1f1efeec
sponce <sebas6nin@gmail.com>
2025-08-06 09:26:01 -0600

    Actualizacion de reporte de antiguedad

--- js/controllers/reciboclictrl.js
+++ js/controllers/reciboclictrl.js
@@ -493,6 +493,7 @@
                 obj.idmoneda = obj.objMoneda != null && obj.objMoneda != undefined ? obj.objMoneda.id : 0;
                 obj.idtipotrans = obj.idtipotrans != null && obj.idtipotrans != undefined ? obj.idtipotrans : 0;
                 obj.monto = obj.monto != null && obj.monto != undefined ? obj.monto : 0.00;
+                obj.tran = obj.tran;
                 return obj;
             }
 
@@ -592,6 +593,7 @@
                     $scope.pagoreccli.idbanco = '2';
                     $scope.pagoreccli.objMoneda = $filter('getById')($scope.lasMonedas, tran.idmoneda);
                     $scope.pagoreccli.monto = tran.monto;
+                    $scope.pagoreccli.tran = idtran;
                 }
             }
 
