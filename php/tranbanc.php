179992526a8667f9f541a7b6d793bbff55dc39da
sponce <sebas6nin@gmail.com>
2025-09-22 15:50:26 -0600

    Agregar lo que habia qiutado por prueba

--- php/tranbanc.php
+++ php/tranbanc.php
@@ -1421,7 +1421,7 @@ $app->post('/tran_pendientes', function () {
                 AND (f.ordentrabajo IS NULL
                 OR f.ordentrabajo = 0) 
                 AND (a.fecha BETWEEN '$d->fdelstr' AND '$d->falstr') 
-                ";
+                AND a.hoja_control = 1 ";
     $query.= isset($d->idempresa) ? "AND f.idempresa = $d->idempresa " : "";
     $query.= isset($d->idproyecto) ? "AND f.idproyecto = $d->idproyecto " : "";
     $query.= isset($d->idproveedor) ? "AND f.idproveedor = $d->idproveedor " : "";
