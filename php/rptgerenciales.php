fa7299bf450682929333f78b212f1bc6b2caf925
sponce <sebas6nin@gmail.com>
2025-10-30 10:04:56 -0600

    Contar las columnas correctas

--- php/rptgerenciales.php
+++ php/rptgerenciales.php
@@ -1097,7 +1097,7 @@ $app->post('/ocupacion', function() {
         $yearObj->promedio_porcentaje_ocupado = round($sum_porcentaje_anual / $num_months_range, 2);
         $yearObj->promedio_total = round($sum_total_anual / $num_months_range, 2);
         $yearObj->promedio_metros_ocupados = round($sum_metros_anual / $num_months_range, 2);
-        $yearObj->columnas = contarMeses($d->mes_del, $d->mes_al) + 1;
+        $yearObj->columnas = contarMeses($d->mes_del, $d->mes_al) + 2;
         $yearObj->columna_tres = floor((contarMeses($d->mes_del, $d->mes_al) + 1) / 4);
 
         array_push($result, $yearObj);
