f4968b8a31871c2a7996af4508d16f9bc824a6da
sponce <sebas6nin@gmail.com>
2026-07-03 00:04:37 +0200

    Retener a pequenios tmb

--- php/compra.php
+++ php/compra.php
@@ -379,7 +379,7 @@ $app->post('/c', function(){
             $calcisr = (int)$db->getOneField("SELECT retensionisr FROM proveedor WHERE id = ".$d->idproveedor) === 1;
 
             // si la empresa es retenedora y el proveedor no es retenedor retener iva
-            if (($empresaRet && !$esRet) && (($d->totfact - $d->noafecto) * $d->tipocambio) >= 2500 && !$esPeque) {
+            if (($empresaRet && !$esRet) && (($d->totfact - $d->noafecto) * $d->tipocambio) >= 2500) {
                 $d->retIva = $db->retIVA((float)$d->iva, 0.15, 1, $esLocalMonedaFact);
             }
         }
@@ -466,7 +466,7 @@ $app->post('/u', function(){
     if ($d->retiva_manual != 1 && !$esNota) {
         if((int)$d->idtipofactura !== 5) {
             // si la empresa es retenedora y el proveedor no es retenedor retener iva
-            if (($empresaRet && !$esRet) && (($d->totfact - $d->noafecto) * $d->tipocambio) >= 2500 && !$esPeque) {
+            if (($empresaRet && !$esRet) && (($d->totfact - $d->noafecto) * $d->tipocambio) >= 2500) {
                 $d->retIva = $db->retIVA((float)$d->iva, 0.15, 1, $esLocalMonedaFact);
             }
         }
