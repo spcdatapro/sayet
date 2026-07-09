7b99bb68d1dd5c8829c35eb7a82664d50672f8c7
sponce <sebas6nin@gmail.com>
2026-07-09 19:55:04 +0200

    Retener a pequenios tmb

--- pln/php/models/Empleado.php
+++ pln/php/models/Empleado.php
@@ -1039,7 +1039,7 @@ class Empleado extends Principal
 			'bdiario' => ($this->lab->bonificacionley / 30)
 		];
 
-		$dias = elemento($args, 'dias_sueldo_pagar', 30);
+		$dias = elemento($args, 'dias_sueldo_pagar', 0);
 
 		if ($dias > 0) {
 			$res['dias']   = $dias;
