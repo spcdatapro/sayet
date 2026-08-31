faa5f7c57de932ea389df12732b00eb6dddd1fb1
sponce <sebas6nin@gmail.com>
2026-08-31 14:00:35 -0600

    Nuevo reporte de documentos descuadrados

--- php/rptreemsinctaliq.php
+++ php/rptreemsinctaliq.php
@@ -7,6 +7,7 @@ require_once 'db.php';
 $app = new \Slim\Slim();
 $app->response->headers->set('Content-Type', 'application/json');
 
+// anterior
 $app->post('/reemsinctaliq', function(){
     $d = json_decode(file_get_contents('php://input'));
     $db = new dbcpm();
@@ -65,6 +66,88 @@ $app->post('/reemsinctaliq', function(){
     }
 
     print json_encode(['generales' => $generales, 'reembolsos' => $reembolsos]);
+});
+
+// nuevo 
+$app->get('/descuadre_reem/:del/:al', function ($del, $al) {
+    $db = new dbcpm;
+
+    $letra = new stdClass();
+    $letra->estampa = new DateTime();
+    $letra->estampa = $letra->estampa->format('d-m-Y H:i');
+
+    $query = "SELECT 
+                e.id AS idempresa,
+                e.nomempresa AS empresa,
+                a.id AS reem,
+                c.iniciales AS usuario,
+                b.fechaingreso AS fecha,
+                b.documento AS factura,
+                b.totfact AS monto,
+                b.conceptomayor AS concepto
+            FROM
+                reembolso a
+                    INNER JOIN
+                compra b ON b.idreembolso = a.id
+                    INNER JOIN
+                usuario c ON b.idusuario = c.id
+                    INNER JOIN
+                (SELECT 
+                    idorigen, SUM(debe) AS debe, SUM(haber) AS haber
+                FROM
+                    detallecontable
+                WHERE
+                    origen = 2
+                GROUP BY idorigen) d ON d.idorigen = b.id
+                    INNER JOIN
+                empresa e ON a.idempresa = e.id
+            WHERE
+                ABS(d.debe - d.haber) > 0
+                    AND a.finicio BETWEEN $del AND $al";
+    $data = $db->getQuery($query);
+
+    $empresas = [];
+    $indexEmpresas = [];
+    $indexReembolsos = [];
+
+    foreach ($data as $fila) {
+        $idEmpresa = (int) $fila->idempresa;
+        $idReem = (int) $fila->reem;
+
+        if (!isset($indexEmpresas[$idEmpresa])) {
+            $empresa = new stdClass();
+            $empresa->idempresa = $idEmpresa;
+            $empresa->empresa = $fila->empresa;
+            $empresa->reembolsos = [];
+            $indexEmpresas[$idEmpresa] = count($empresas);
+            $empresas[] = $empresa;
+        }
+
+        $empresaIndex = $indexEmpresas[$idEmpresa];
+        $empresaObj = &$empresas[$empresaIndex];
+
+        if (!isset($indexReembolsos[$idEmpresa . ':' . $idReem])) {
+            $reembolso = new stdClass();
+            $reembolso->reem = $idReem;
+            $reembolso->facturas = [];
+            $indexReembolsos[$idEmpresa . ':' . $idReem] = count($empresaObj->reembolsos);
+            $empresaObj->reembolsos[] = $reembolso;
+        }
+
+        $reemIndex = $indexReembolsos[$idEmpresa . ':' . $idReem];
+        $reembolsoObj = &$empresaObj->reembolsos[$reemIndex];
+
+        $factura = new stdClass();
+        $factura->usuario = $fila->usuario;
+        $factura->fecha = $fila->fecha;
+        $factura->factura = $fila->factura;
+        $factura->monto = (float) $fila->monto;
+        $factura->concepto = $fila->concepto;
+
+        $reembolsoObj->facturas[] = $factura;
+    }
+
+    print json_encode(['encabezado' => $letra, 'empresas' => $empresas]);
 });
 
 $app->run();
