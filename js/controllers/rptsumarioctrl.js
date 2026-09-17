f3543ff790b8e28860001bedb789715eade32c4d
sponce <sebas6nin@gmail.com>
2026-09-17 11:35:37 -0600

    Mejoras reporte libro diario

--- js/controllers/rptsumarioctrl.js
+++ js/controllers/rptsumarioctrl.js
@@ -4,7 +4,7 @@
 
     rptsumarioctrl.controller('rptSumarioCtrl', ['$scope', 'jsReportSrvc', 'monedaSrvc', 'bancoSrvc', '$sce', '$http', '$window', '$q', '$filter', 'Upload', 'authSrvc', function ($scope, jsReportSrvc, monedaSrvc, bancoSrvc, $sce, $http, $window, $q, $filter, Upload, authSrvc) {
 
-        $scope.params = { fecha: moment().toDate(), idmoneda: '1', solomov: 1, tipo: '1' };
+        $scope.params = { fecha: moment().toDate(), idmoneda: '1', solomov: 1, tipo: '1', pormes: '0' };
         $scope.content = '';
         // variable para mostrar que esta cargando
         $scope.estaGenerando = false;
